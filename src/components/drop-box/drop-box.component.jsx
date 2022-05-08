0ae3a896199bc7b2c946c156884018bf97c1c742
Luciano Muniz <luciano.mo@gmail.com>
2022-05-08 11:08:08 -0300

    modify the DropBox component and populate with the data

--- src/components/drop-box/drop-box.component.jsx
+++ src/components/drop-box/drop-box.component.jsx
@@ -15,23 +15,4 @@ const DropBox = ({ className, values, onChangeHandler, placeholder }) => (
   </div>
 );
 
-// (
-//   <div>
-//     {placeholder}:
-//     <select
-//       className={`drop-box ${className}`}
-//       onChange={onChangeHandler}>
-//       <option value="">-- Select a {placeholder}</option>
-
-//       {
-//         values.map((valueReg) => {
-//           <option key={valueReg.id} value={valueReg.value}>{valueReg.label}</option>
-//         })
-//       }
-
-//       </select>
-//     />
-//   </div>
-// );
-
 export default DropBox;
